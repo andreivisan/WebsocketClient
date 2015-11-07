47a0679045bf9a12ece8c6d275a2ca378477e12f
Andrei Visan <andreivisan@andreis-mbp.home>
2015-11-07 19:27:36 +0100

    Partial commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var WebSocket = require('ws');
 var wsClient = require('../ws-client/index')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  wsClient.createWebSocketClient();
+  //wsClient.createWebSocketClient();
 
-  res.render('index', { title: 'Express' });
+  var ws = new WebSocket('ws://192.168.2.26:8080/stream');
+
+  ws.on('open', function open() {
+    ws.send('test');
+  });
+
+  ws.on('message', function(data, flags) {
+    console.log("Socket response arrived!");
+    res.render('index', { title: 'Express', image:data});
+  });
+
 });
 
 module.exports = router;
